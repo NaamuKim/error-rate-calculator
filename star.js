76358530c1d2644e3cd46a04e0242268bac4cc91
lost731 <lost731@naver.com>
2021-10-17 23:14:20 +0900

    Add: complete

--- star.js
+++ star.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("form");
 const  tv = document.getElementById("tv");
-const  rv = document.getElementById("rv");
+const  dv = document.getElementById("dv");
 
 let result = document.getElementById("result");
 
@@ -8,10 +8,10 @@ let result = document.getElementById("result");
 function onSubmit(event) {
     event.preventDefault();
     let theoryVal=tv.value;
-    let realVal=rv.value;
+    let realVal=dv.value;
     let res=(theoryVal-realVal)/theoryVal*100;
     console.log(res);
-    result.innerHTML= `오차율은 ${res}입니다`;
+    result.innerHTML='오차율은 ' + res +'%입니다';
 }
     form.addEventListener("submit", onSubmit );
 
